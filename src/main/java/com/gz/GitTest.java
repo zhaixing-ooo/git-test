fdfb08bebe327f314fce28cdcf1806c0caaaf0a1
zhaixing <19939932443@163.com>
2023-08-28 12:06:08 +0800

    second commit

--- src/main/java/com/gz/GitTest.java
+++ src/main/java/com/gz/GitTest.java
@@ -4,6 +4,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitTest");
         System.out.println("gitTest master second commit");
-        System.out.println("gitTest second commit");
     }
 }
