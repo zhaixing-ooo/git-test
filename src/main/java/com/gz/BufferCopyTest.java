9bc15230b269b9815b5de0b2f893018e7e2f1357
zhaixing <19939932443@163.com>
2023-08-30 13:09:53 +0800

    FILE3 COMMIT

---
+++ src/main/java/com/gz/BufferCopyTest.java
@@ -0,0 +1,38 @@
+package com.gz;
+
+import java.io.*;
+
+public class BufferCopyTest {
+    public class Demo_BufferedStream {
+        /*
+         * BufferedInputStream    字节缓冲输入流
+         * BufferedOutputStream   字节缓冲输出流
+         *
+         */
+        public static void main(String[] args){
+            BufferedInputStream bis = null;
+            BufferedOutputStream bos = null;
+            try {
+                //1.创建字节缓冲，输入输出流
+                bis = new BufferedInputStream(new FileInputStream("凄美地.txt"));
+                bos = new BufferedOutputStream(new FileOutputStream("COPY.txt"));
+                //2.读写操作
+                int i;
+                while ((i = bis.read()) != -1) {
+                    bos.write(i);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }finally {
+                try {
+                    //3.关闭流释放资源
+                    if(bis != null)bis.close();
+                    if(bos != null)bos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+    }
+}
