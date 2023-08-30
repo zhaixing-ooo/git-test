4cac7994c03438ef9367b18c286bba35bd17979f
zhaixing <19939932443@163.com>
2023-08-30 13:04:52 +0800

    FILE2 COMMIT

--- src/main/java/com/gz/FisCopyTest.java
+++ src/main/java/com/gz/FisCopyTest.java
@@ -9,8 +9,8 @@ public class FisCopyTest {
         try {
             int i ;
            fis = new FileInputStream(new File("凄美地.txt"));
-            fos = new FileOutputStream(new File("COPY.txt"));
-            byte[] bys = new byte[1024];
+            fos = new FileOutputStream(new File("COPY.txt"));//字节输入输出流
+            byte[] bys = new byte[1024];//字符缓冲数组（大幅提高拷贝效率）
             int len;
             while((len = fis.read(bys))!=-1){
                 fos.write(bys,0,len);
